5277f0456a2dcd3de4cb0780e7632467d57ad9fd
toov00 <97659040+toov00@users.noreply.github.com>
2025-11-17 13:37:52 +0800

    Reduce window size

--- src/main.js
+++ src/main.js
@@ -9,8 +9,8 @@ let mainWindow;
 
 function createWindow() {
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 400,
+    height: 400,
     webPreferences: {
       nodeIntegration: false, 
       contextIsolation: true,
